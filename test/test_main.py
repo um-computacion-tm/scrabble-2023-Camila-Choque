c85085a75f2b1c7f67f264847999ac21e530f753
Camila-Choque <m.choque@alumno.um.edu.ar>
2023-10-24 22:39:50 -0300

    commit 30

--- test/test_main.py
+++ test/test_main.py
@@ -13,12 +13,14 @@ def test_take_turn_player_play(self, mock_input, mock_print, mock_object):
         player.rack = [Tile('H', 4), Tile('O', 1), Tile('L', 1), Tile('A', 1)]
         main.take_turn()
 
+
 def test_exchange_tiles(self, mock_input):
         main = Main()
         main.game.players[0].rack = [Tile('H', 4), Tile('O',1), Tile('L',1), Tile('A',1)]
         main.game.next_turn()
         main.exchange_tiles()
         self.assertEqual(len(main.game.players[0].rack), 4)
+
 
 def test_change_joker_to_tile_true(self, mock_input):
         main = Main()
